60c7fde4de3f6c9ff7e188755ad1cf38641cf700
gcaanmm-maker <gcaanmm@gmail.com>
2026-05-15 19:52:14 -0300

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,1 +1,7 @@
-q
+const SUPABASE_URL = 'https://gaeftsnshlkkssqbokhh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhZWZ0c25zaGxra3NzcWJva2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4Njk3MTUsImV4cCI6MjA5NDQ0NTcxNX0.TY2JQsia_MoBnBJaomaWCw51R-ZUfrutGhDzrfkpoTM';
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
