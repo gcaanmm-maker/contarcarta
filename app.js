f22716a6083cc3b2377bb32d767de92d2be7adc7
gcaanmm-maker <gcaanmm@gmail.com>
2026-05-15 19:38:27 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+let playerId = crypto.randomUUID();
+
+    div.className = 'played-card';
+
+    div.innerText = card.value;
+
+    container.appendChild(div);
+  });
+}
+
+
+async function loadRoomInfo() {
+
+  const { data } = await supabaseClient
+    .from('rooms')
+    .select('*')
+    .eq('code', roomCode)
+    .single();
+
+  document.getElementById('level').innerText = data.level;
+  document.getElementById('lives').innerText = data.lives;
+}
+
+
+function listenRealtime() {
+
+  supabaseClient
+    .channel('players-channel')
+    .on(
+      'postgres_changes',
+      {
+        event: '*',
+        schema: 'public',
+        table: 'players'
+      },
+      () => {
+        loadPlayers();
+      }
+    )
+    .subscribe();
+
+
+  supabaseClient
+    .channel('played-channel')
+    .on(
+      'postgres_changes',
+      {
+        event: '*',
+        schema: 'public',
+        table: 'played_cards'
+      },
+      () => {
+        loadPlayedCards();
+      }
+    )
+    .subscribe();
+}
